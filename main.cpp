cb045efbdedd040189a6bc35f087808a2fcfd4db
Stef Busking <stef@scivis.net>
2009-09-18 14:55:05 +0000

    Use a VTK output window we can actually read if the app crashes.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 #include <QApplication>
 #include "Gui/MainWindow.h"
 
+#include <vtkWin32ProcessOutputWindow.h>
+
 int main(int argc, char *argv[]) 
 {
+#ifdef WIN32
+	// Use a more useful output window for VTK
+	vtkWin32ProcessOutputWindow *ow = vtkWin32ProcessOutputWindow::New();
+	vtkWin32ProcessOutputWindow::SetInstance(ow);
+	ow->Delete();
+#endif
+
 	// Set up Qt application
 	QApplication app(argc, argv);
 
