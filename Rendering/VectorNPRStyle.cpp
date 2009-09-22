1dc6bc458ce722be343c1681adee941e2743139a
Stef Busking <stef@scivis.net>
2009-09-22 09:22:56 +0000

    Load shaders relative to application path.

--- Rendering/VectorNPRStyle.cpp
+++ Rendering/VectorNPRStyle.cpp
@@ -6,6 +6,8 @@
 
 #include "Shaders.h"
 
+#include <QApplication>
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -15,13 +17,30 @@ namespace VFE
 {
 	// ------------------------------------------------------------------------
 	// Helper function
-	std::string LoadTextFile(const std::string &filename)
+	std::string LoadShader(const std::string &filename)
 	{
-		std::ifstream infile(filename.c_str());
+		// TODO: embed shader sources in the app for final version
+		std::string apppath = QApplication::applicationDirPath().toStdString();
+		// Check up to 2 levels up in the directory hierarchy
+		std::string searchpath = "/Shaders/" + filename;
+		std::ifstream infile((apppath + searchpath).c_str());
+		for (int i = 0; i < 2 && !infile; ++i)
+		{
+			searchpath =  "/.." + searchpath;
+			infile.close();
+			infile.clear();
+			infile.open((apppath + searchpath).c_str());
+		}
+		std::cout << "Loading shader '" << apppath + searchpath << 
+			"'..." << std::endl;
 		std::ostringstream contents;
 		if (infile.good())
 		{
 			contents << infile.rdbuf();
+		}
+		else
+		{
+			std::cerr << "Error loading shader!" << std::endl;
 		}
 		return contents.str();
 	}
@@ -42,12 +61,10 @@ namespace VFE
 		GLProgram *scribe = GLProgram::New();
 		// Scribe vertex shaders
 		bool res = scribe->AddVertexShader(
-			AddShaderDefines(LoadTextFile(
-			"D:\\Temp\\VFE\\TestScribeVS.txt")));
+			AddShaderDefines(LoadShader("VFENPRScribeVS.txt")));
 		// Scribe fragment shaders
 		if (res) res = scribe->AddFragmentShader(
-			AddShaderDefines(LoadTextFile(
-			"D:\\Temp\\VFE\\TestScribeFS.txt")));
+			AddShaderDefines(LoadShader("VFENPRScribeFS.txt")));
 		if (res) res = scribe->AddFragmentShader(
 			AddShaderDefines(Shaders::LibUtility));
 		if (res) res = scribe->Link();
@@ -66,12 +83,10 @@ namespace VFE
 		GLProgram *raycaster = GLProgram::New();
 		// raycaster vertex shaders
 		bool res = raycaster->AddVertexShader(
-			AddShaderDefines(LoadTextFile(
-			"D:\\Temp\\VFE\\TestRaycasterVS.txt")));
+			AddShaderDefines(LoadShader("VFENPRRaycasterVS.txt")));
 		// raycaster fragment shaders
 		if (res) res = raycaster->AddFragmentShader(
-			AddShaderDefines(LoadTextFile(
-			"D:\\Temp\\VFE\\TestRaycasterFS.txt")));
+			AddShaderDefines(LoadShader("VFENPRRaycasterFS.txt")));
 		if (res) res = raycaster->AddFragmentShader(
 			AddShaderDefines(Shaders::LibUtility));
 		if (res) res = raycaster->Link();
@@ -90,12 +105,10 @@ namespace VFE
 		GLProgram *painter = GLProgram::New();
 		// painter vertex shaders
 		bool res = painter->AddVertexShader(
-			AddShaderDefines(LoadTextFile(
-			"D:\\Temp\\VFE\\TestPainterVS.txt")));
+			AddShaderDefines(LoadShader("VFENPRPainterVS.txt")));
 		// painter fragment shaders
 		if (res) res = painter->AddFragmentShader(
-			AddShaderDefines(LoadTextFile(
-			"D:\\Temp\\VFE\\TestPainterFS.txt")));
+			AddShaderDefines(LoadShader("VFENPRPainterFS.txt")));
 		if (res) res = painter->AddFragmentShader(
 			AddShaderDefines(Shaders::LibUtility));
 		if (res) res = painter->Link();
