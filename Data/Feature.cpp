1b99bc2bbb4153a51259f49da3240fe75dc41342
Stef Busking <stef@scivis.net>
2009-09-30 16:08:56 +0000

    More preparation for defining Features in the GUI.

--- Data/Feature.cpp
+++ Data/Feature.cpp
@@ -1,7 +1,96 @@
 #include "Feature.h"
 
+#include <GLBlaat/GLProgram.h>
+
+#include <sstream>
+
 namespace VFE
 {
 	// ------------------------------------------------------------------------
-	// ...
+	Property::Property(const std::string &name, PropertyType type)
+		: name(name), type(type) 
+	{
+	}
+
+	// ------------------------------------------------------------------------
+	int Property::GetSize() const
+	{
+		switch (type)
+		{
+		case Float:
+			return 1;
+			break;
+		case Vec3:
+			return 3;
+			break;
+		default:
+			return 0;
+		}
+	}
+
+	// ------------------------------------------------------------------------
+	std::vector<Property> FeatureVector::properties = std::vector<Property>();
+
+	// ------------------------------------------------------------------------
+	FeatureVector::FeatureVector()
+	{
+		if (properties.size() == 0)
+		{
+			properties.push_back(Property("position", Property::Vec3));
+			properties.push_back(Property("vector", Property::Vec3));
+			properties.push_back(Property("direction", Property::Vec3));
+			properties.push_back(Property("magnitude", Property::Float));
+			properties.push_back(Property("growth", Property::Float));
+			properties.push_back(Property("divergence", Property::Float));
+			properties.push_back(Property("curl", Property::Vec3));
+		}
+
+		// Compute total size
+		int totalSize = 0;
+		for (std::vector<Property>::const_iterator it = properties.begin(); 
+			it != properties.end(); ++it)
+		{
+			totalSize += it->GetSize();
+		}
+		values.resize(totalSize, 0.0f);
+	}
+
+	// ------------------------------------------------------------------------
+	void FeatureVector::SetupProgram(GLProgram *program, 
+		const std::string &feature)
+	{
+		int v = 0;
+		for (std::vector<Property>::const_iterator it = properties.begin(); 
+			it != properties.end(); ++it)
+		{
+			switch (it->type)
+			{
+			case Property::Float:
+				program->SetUniform1f(feature + it->name, values[v]);
+				break;
+			case Property::Vec3:
+				program->SetUniform3f(feature + it->name, 
+					values[v], values[v + 1], values[v + 2]);
+				break;
+			}
+			v += it->GetSize();
+		}
+	}
+
+	// ------------------------------------------------------------------------
+	void Feature::SetupProgram(GLProgram *program, int index)
+	{
+		// Get the name of this feature
+		std::ostringstream feature;
+		feature << "userFeatures[" << index << "].";
+		program->SetUniform1i(feature.str() + "enabled", enabled ? 1 : 0);
+		program->SetUniform3f(feature.str() + "examplePos", 
+			static_cast<float>(examplePos.x), 
+			static_cast<float>(examplePos.y), 
+			static_cast<float>(examplePos.z));
+		mask.SetupProgram(program, feature.str() + "mask.");
+		program->SetUniform1f(feature.str() + "startThreshold", 
+			startThreshold);
+		program->SetUniform1f(feature.str() + "endThreshold", endThreshold);
+	}
 }
--- Data/Feature.h
+++ Data/Feature.h
@@ -1,8 +1,52 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include <NQVTK/Math/Vector3.h>
+
+class GLProgram;
+
 namespace VFE
 {
+	class Property
+	{
+	public:
+		std::string name;
+		enum PropertyType
+		{
+			Float, 
+			Vec3
+		} type;
+
+		Property(const std::string &name, PropertyType type);
+
+		int GetSize() const;
+	};
+
+	class FeatureVector
+	{
+	public:
+		FeatureVector();
+
+		void SetupProgram(GLProgram *program, const std::string &feature);
+
+	protected:
+		// Property descriptions are shared by all FeatureVectors
+		static std::vector<Property> properties;
+		std::vector<float> values;
+	};
+
 	class Feature
 	{
+	public:
+		bool enabled;
+		NQVTK::Vector3 examplePos;
+		FeatureVector mask;
+		float startThreshold;
+		float endThreshold;
+		// TODO: add visualization style parameters
+
+		void SetupProgram(GLProgram *program, int index);
 	};
 }
